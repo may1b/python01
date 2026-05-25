e9e886f3286f3d6e9ef516725e42cf54271ec304
May <magrass@student.42heilbronn.de>
2026-05-25 15:33:13 +0200

    done ex0!

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,12 @@
+def main():
+    name = "Rose"
+    height = 25 # cm
+    age = 30 # days
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days", end="\n\n")
+    print("=== End of Program ===")
+
+if __name__ == "__main__":
+    main()
