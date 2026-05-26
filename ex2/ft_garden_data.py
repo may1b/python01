ec1431f386eb18093e12337d0d9a5ded559950f6
May <magrass@student.42heilbronn.de>
2026-05-26 14:59:26 +0200

    done ex2!

---
+++ ex2/ft_garden_data.py
@@ -0,0 +1,43 @@
+class Plant:
+    _name: str
+    _height: float  # in cm
+    _age: int  # in days
+
+    def __init__(self, name: str, height: float, age: int):
+        self._name = name
+        self._height = height
+        self._age = age
+
+    def show(self):
+        day_s = "days" if self._age > 1 else "day"
+        print(f"{self._name}: {round(self._height, 1)}cm,\
+ {self._age} {day_s} old")
+
+    def grow(self):
+        self._height += 0.8
+
+    def age(self):
+        self._age += 1
+
+    def age_and_grow(self):
+        self.grow()
+        self.age()
+
+
+def main():
+    starting_height = 25.0
+    rose = Plant("Rose", starting_height, 30)
+    print("=== Garden Plant Growth ===")
+
+    day = 1
+    while day <= 7:
+        print(f"=== Day {day} ===")
+        rose.show()
+        rose.age_and_grow()
+        day += 1
+
+    print(f"Growth this week: {round(rose._height - starting_height)}cm")
+
+
+if __name__ == "__main__":
+    main()
